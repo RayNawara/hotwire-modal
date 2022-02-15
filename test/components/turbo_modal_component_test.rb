ecac0e7dd491d4fa044a0d36d813fcab2167a0d0
RayNawara <rjnawara@gmail.com>
2022-02-15 12:45:57 -0500

    View component turbo modal

---
+++ test/components/turbo_modal_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class TurboModalComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(TurboModalComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
